589e3930cf58c5ec85027bf2862439011a57e5ad
johny758 <jk211@hdm-stuttgart.de>
2021-12-29 12:00:32 +0100

    Webinterface now working with local truffle Blockchain

--- Web/truffle-config.js
+++ Web/truffle-config.js
@@ -8,5 +8,12 @@ module.exports = {
     develop: {
       port: 8545
     }
+  },
+  compilers: {
+    solc: {
+      version: '^0.6.0', // A version or constraint - Ex. "^0.5.0"
+                         // Can be set to "native" to use a native solc or
+                         // "pragma" which attempts to autodetect compiler version
+    }
   }
 };
